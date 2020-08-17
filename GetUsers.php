fb2b832a920d71c73b4b6a70e515de265d59cfcb
hmha4 <4Makarl@gmail.com>
2020-08-17 13:16:18 +0900

    test
    test

---
+++ GetUsers.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "0000";
+$dbname = "testlogin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully, now we will show the users";
+
+$sql = "SELECT id, username, password, level, coins FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<br>". "id: " . $row["id"]
+        ." - username: " . $row["username"]
+        ." - password: " . $row["password"]
+        ." - level: " . $row["level"]
+        ." - coins: " . $row["coins"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+
+?>
